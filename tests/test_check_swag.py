e08b2ae0aaf3dbcfa671bb58d143bb0287653726
Dmitriy <dimon798master@gmail.com>
2025-05-11 23:59:21 +0300

    1

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -1,6 +1,7 @@
 # from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+
 # def test_icon_exist():
 #     driver = webdriver.Chrome()
 #     driver.get("https://www.saucedemo.com/")
