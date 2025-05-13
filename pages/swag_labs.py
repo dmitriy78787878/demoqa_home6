85314ab35769f4b5037adf2216b044dcca640338
Dmitriy <dimon798master@gmail.com>
2025-05-13 21:09:13 +0300

    3

--- pages/swag_labs.py
+++ pages/swag_labs.py
@@ -24,11 +24,3 @@ class SwagLabs(BasePage):
             return False
         return True
 
-    # def click_on_the_icon(self):
-    #     self.find_element(locator='#app > header > a').click()
-    #
-    # def equal_url(self):
-    #     if self.get_url() == 'https://demoqa.com/':
-    #         return True
-    #     else:
-    #         return False
--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -1,31 +1,7 @@
-# from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-
-# def test_icon_exist():
-#     driver = webdriver.Chrome()
-#     driver.get("https://www.saucedemo.com/")
-#     icon = driver.find_element(By.CSS_SELECTOR, '#app > header > a')
-#
-#     if icon is None:
-#         print('Элемент не найден')
-#     else:
-#         print('Элемент найден')
-
 from pages.swag_labs import SwagLabs
-
 import time
 
 def test_check_swag(browser):
-#     browser.get("https://www.saucedemo.com/")
-#
-#     icon = browser.find_element(By.CSS_SELECTOR, 'div.login_logo')
-#
-#     if icon is None:
-#         print('Элемент не найден')
-#     else:
-#         print('Элемент найден')
-
 
     demo_qa_page = SwagLabs(browser)
     demo_qa_page.visit()
@@ -36,10 +12,3 @@ def test_check_swag(browser):
     time.sleep(3)
     assert demo_qa_page.exist_password()
 
-
-    # # time.sleep(3)
-    # demo_qa_page.click_on_the_icon()
-    # # time.sleep(3)
-    # assert demo_qa_page.equal_url()
-
-    # assert demo_qa_page.equal_url()
